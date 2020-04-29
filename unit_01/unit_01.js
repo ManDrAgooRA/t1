d1a7c3105185ecae5d1f22a9e444c899781922ae
zordan <zekagorobey@gmail.com>
2020-04-30 00:12:49 +0300

    first home work push

---
+++ unit_01/unit_01.js
@@ -0,0 +1,113 @@
+// Task 1.
+console.log('Евгений');
+
+// Task 2.
+// Выведите в консоль номер месяца в котором вы родились. Изучите чем отличается вывод числа и строки.
+
+console.log(06);
+
+
+
+// Task 3.
+// Выведите в консоль строку:
+// 'Добро '+'пожаловать '+' на курс'
+
+console.log('Добро ' + 'пожаловать ' + ' на курс');
+
+
+// Task 4.
+// alert(2019);
+
+
+
+// Task 5.
+// С помощью alert выведите результат операции:
+// alert(2019 - 200);
+
+
+
+// Task 6.
+document.getElementById('one').innerHTML = 'Hello World';
+
+
+
+// Task 7.
+document.getElementById('two').innerHTML = 12 * 12;
+
+
+// Task 8.
+document.querySelector('.one').innerHTML = 'Hello World'
+
+
+// Task 9.
+document.querySelector('h2 span').innerHTML = 'world';
+
+
+
+// Task 10.
+document.querySelector('.three').innerHTML = "<h3>Hello World</h3>";
+
+
+
+// Task 11.
+document.querySelector('.four').innerHTML = "<h4>Hello</h4>";
+
+document.querySelector('.four').innerHTML += "<p>World</p>";
+
+
+
+// Task 12.
+let a = document.querySelector('.five');
+a.innerHTML = 3.1415;
+
+
+
+// Task 13.
+let div7 = document.querySelector('.seven');
+div7.innerHTML = '<img src="https://cdn4.iconfinder.com/data/icons/food-and-drink-flat-gradient/32/cone_ice_cream_food_drink_sweet-512.png" alt="">';
+
+
+// Task 14.
+let z1 = 6;
+let z2 = 3;
+let out = document.querySelector('.out-14');
+out.innerHTML = z1 * z2;
+
+
+// Task 15.
+let y1 = 6;
+let y2 = 3;
+let exit = document.querySelector('.out-15');
+exit.innerHTML = y1 / y2;
+
+
+// Task 16.
+let x1 = 'Hello';
+let x2 = 5;
+let conct = document.querySelector('.out-16');
+conct.innerHTML = x1 + x2;
+
+
+
+
+// Task 17.
+let d1 = document.querySelector('.test-1');
+console.log(d1);
+
+
+// Task 18.
+let d2 = document.querySelector('.test-2');
+console.log(d2);
+d2 = 5;
+console.log(d2);
+
+// Task 19.
+let divS3 = document.querySelector('.s3');
+console.log(divS3);
+divS3 = document.querySelector('.s4');
+console.log(divS3);
+
+
+// Task 20
+// let body = document.querySelector('body');
+// body.innerHTML = "";
